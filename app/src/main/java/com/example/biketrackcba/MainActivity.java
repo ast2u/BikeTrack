a968159441e383c29e7a4f0bff243a3c24393825
Carlo <carloasto11@gmail.com>
2023-09-17 19:02:11 +0800

    -

--- app/src/main/java/com/example/biketrackcba/MainActivity.java
+++ app/src/main/java/com/example/biketrackcba/MainActivity.java
@@ -14,6 +14,7 @@ private Button lgEmail, lgGmail;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         lgEmail=findViewById(R.id.loginEmail);
+
         lgEmail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
