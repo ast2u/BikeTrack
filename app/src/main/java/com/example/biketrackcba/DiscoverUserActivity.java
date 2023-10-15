612dec60dcd8cd44d5438a8767327478e0ef25b5
cortzhrt <natsukisubaruarcadia13@gmail.com>
2023-10-15 20:31:14 +0800

    update cortez october 15

--- app/src/main/java/com/example/biketrackcba/DiscoverUserActivity.java
+++ app/src/main/java/com/example/biketrackcba/DiscoverUserActivity.java
@@ -2,6 +2,8 @@ package com.example.biketrackcba;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -11,13 +13,54 @@ import android.view.MenuItem;
 import android.widget.SearchView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class DiscoverUserActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
-private BottomNavigationView bottomNavigationView;
+
+    RecyclerView recyclerView;
+    DatabaseReference database;
+    adapter adapter;
+    ArrayList<routesinfo> list;
+
+    private BottomNavigationView bottomNavigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_discover_user);
+
+        recyclerView = findViewById(R.id.userlist);
+        database = FirebaseDatabase.getInstance().getReference("Routes");
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        list = new ArrayList<>();
+        adapter = new adapter(this,list);
+        recyclerView.setAdapter(adapter);
+
+        database.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    routesinfo routesinfo = dataSnapshot.getValue(routesinfo.class);
+                    list.add(routesinfo);
+                }
+                adapter.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         bottomNavigationView = findViewById(R.id.bottomNavView);
         Menu menu = bottomNavigationView.getMenu();
         bottomNavigationView.setSelectedItemId(R.id.miDiscover);
@@ -26,6 +69,9 @@ private BottomNavigationView bottomNavigationView;
 
         SearchView searchView = findViewById(R.id.searchView);
         searchView.setOnQueryTextListener(this);
+
+        FirebaseApp.initializeApp(this);
+
 
 
         bottomNavigationView.setOnItemSelectedListener(item -> {
---
+++ app/src/main/java/com/example/biketrackcba/routesinfo.java
@@ -0,0 +1,19 @@
+package com.example.biketrackcba;
+//TONG CLASS NA TO IS PARA SA INFO PARA KAY ROUTES WAG DEDELETE
+public class routesinfo {
+
+    String desc, points, title;
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public String getPoints() {
+        return points;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+}
