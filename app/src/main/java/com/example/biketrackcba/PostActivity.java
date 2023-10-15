28ccd9a5484dac0a12823ec36e1b5d3606cd646d
quelzarth <brosennjeimar@yahoo.com>
2023-10-15 19:26:06 +0800

    socials update part 2

--- app/src/main/java/com/example/biketrackcba/PostActivity.java
+++ app/src/main/java/com/example/biketrackcba/PostActivity.java
@@ -1,22 +1,43 @@
 package com.example.biketrackcba;
 
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
 import android.view.View;
+import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 import android.widget.VideoView;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 public class PostActivity extends AppCompatActivity {
 
@@ -35,6 +56,7 @@ public class PostActivity extends AppCompatActivity {
 
     MediaController mediaController;
     String type;
+    ActivityResultLauncher<String> mTakePhoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,14 +70,25 @@ public class PostActivity extends AppCompatActivity {
         videoView = findViewById(R.id.vv_post);
         btnchoosefile = findViewById(R.id.btn_choosefile_post);
         btnuploadfile = findViewById(R.id.btn_uploadfile_post);
-        etdesc = findViewById(R.id.et_desc_post);
-        db1 = database.getReference("All images");
-        db2 = database.getReference("All videos");
-        db3 = database.getReference("All posts");
+        mTakePhoto = registerForActivityResult(
+                new ActivityResultContracts.GetContent(),
+                new ActivityResultCallback<Uri>() {
+                    @Override
+                    public void onActivityResult(Uri result) {
+                        imageView.setImageURI(result);
+                    }
+                }
+        );
+
+        storageReference = FirebaseStorage.getInstance().getReference("User posts");
 
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         String currentuid = user.getUid();
 
+        etdesc = findViewById(R.id.et_desc_post);
+        db1 = database.getReference("All images").child(currentuid);
+        db2 = database.getReference("All videos").child(currentuid);
+        db3 = database.getReference("All posts");
         btnuploadfile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -66,18 +99,100 @@ public class PostActivity extends AppCompatActivity {
         btnchoosefile.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                chooseImage();
+                mTakePhoto.launch("image/* video/*");
             }
         });
     }
 
-    private void chooseImage() {
-        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-        intent.setType("image/* video/*");
-        //intent.setAction(Intent.ACTION_GET_CONTENT);
-        startActivityForResult(intent,PICK_FILE);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_FILE || requestCode == RESULT_OK ||
+                data != null || data.getData() != null){
+            selectedUri = data.getData();
+            if (selectedUri.toString().contains("image")){
+                Picasso.get().load(selectedUri).into(imageView);
+                imageView.setVisibility(View.VISIBLE);
+                videoView.setVisibility(View.INVISIBLE);
+                type = "iv";
+            }else if (selectedUri.toString().contains("video")){
+                videoView.setMediaController(mediaController);
+                videoView.setVisibility(View.VISIBLE);
+                imageView.setVisibility(View.INVISIBLE);
+                videoView.setVideoURI(selectedUri);
+                videoView.start();
+                type = "vv";
+            }else {
+                Toast.makeText(this, "No file selected", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+    private String getFileExt(Uri uri){
+        ContentResolver contentResolver = getContentResolver();
+        MimeTypeMap mimeTypeMap = MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType((contentResolver.getType(uri)));
     }
 
-    private void Dopost() {
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentuid = user.getUid();
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        DocumentReference documentReference = db.collection("user").document(currentuid);
+
+        documentReference.get()
+                .addOnCompleteListener((task) -> {
+
+                    if (task.getResult().exists()){
+                        name = task.getResult().getString("name");
+                        url = task.getResult().getString("url");
+                    }else {
+                        Toast.makeText(PostActivity.this, "error", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+
+    void Dopost() {
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        String currentuid = user.getUid();
+
+        String desc = etdesc.getText().toString();
+
+        Calendar cdate = Calendar.getInstance();
+        SimpleDateFormat currentdate = new SimpleDateFormat("MMMM-dd-yyyy");
+        final String savedate = currentdate.format(cdate.getTime());
+
+        Calendar ctime = Calendar.getInstance();
+        SimpleDateFormat currenttime = new SimpleDateFormat("HH:mm:ss");
+        final String savetime = currenttime.format(ctime.getTime());
+
+        String time = savedate +":"+ savedate;
+
+        if (TextUtils.isEmpty(desc) || selectedUri != null){
+            Uri imageUri;
+            final StorageReference reference = storageReference.child(System.currentTimeMillis()+"."+getFileExt(selectedUri));
+            uploadTask = reference.putFile(selectedUri);
+
+            Task<Uri> uriTask = uploadTask.continueWithTask((task) -> {
+                if (!task.isSuccessful()){
+                    throw task.getException();
+                }
+                return reference.getDownloadUrl();
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()){
+                        Uri downloadurl = task.getResult();
+
+
+                    }
+                }
+            });
+        } else {
+            Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show();
+        }
     }
 }
